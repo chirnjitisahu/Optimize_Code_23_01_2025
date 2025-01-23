84c136d35610acad59ef609230c74cccd7d01c72
chirnjiti sahu <chirnjiti.sahu@zehntech.com>
2025-01-23 16:43:46 +0530

    latest changes

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:core';
-// void main() {
-//   // Create a stopwatch instance to track time
-//   Stopwatch stopwatch = Stopwatch();
 
-//   // Start the stopwatch
-//   stopwatch.start();
-
-//   // Optimized loop to print even numbers from 2 to 100
-//   for (int i = 1; i <= 50; i++) {
-//     print(i * 2);
-//   }
-
-//   // Stop the stopwatch
-//   stopwatch.stop();
-
-//   // Print the elapsed time in milliseconds
-//   print('Time taken: ${stopwatch.elapsedMicroseconds} milliseconds');
-// }
 void main() {
   // Start the stopwatch to measure the time
  for (int i = 1; i <= 25; i++) {
