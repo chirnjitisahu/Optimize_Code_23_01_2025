ea6bf6cc04191bbbe92efe665bea50fa077e7051
chirnjiti sahu <chirnjiti.sahu@zehntech.com>
2025-01-23 16:42:32 +0530

    push

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -20,32 +20,10 @@ import 'dart:core';
 // }
 void main() {
   // Start the stopwatch to measure the time
-  Stopwatch stopwatch = Stopwatch();
-  stopwatch.start();
-// -----------First Method : Loop run 50 times------------------------------------
-//This is the optimized code
-  //   for (int i = 1; i <= 50; i++) {
-//     print(i * 2);
-//   }
-
-
-//  ---------------------Loop to print even numbers from 1 to 100 (25 iterations)------------------
-  // for (int i = 1; i <= 25; i++) {
-  //   // Print even numbers from 1-50
-  //   print(2 * i);
-  //   // Print even numbers from 51-100
-  //   print(2 * (i + 25));
-  // }
-  //Here's a way to loop only 25 times and still print all even numbers up to 100:
-  //  for (int i = 1; i <= 25; i++) {
-  //   print(i * 4 - 2); // First even number
-  //   print(i * 4);     // Second even number
-  // }
- 
- 
-  // Stop the stopwatch after the loop is done
-  stopwatch.stop();
- 
-  // Output the elapsed time
-  print('Execution Time: ${stopwatch.elapsedMicroseconds} elapsedMicroseconds');
+ for (int i = 1; i <= 25; i++) {
+      // Print even numbers from 1-50
+      print(2 * i);
+      // Print even numbers from 51-100
+      print(2 * (i + 25));
+    }
 }
